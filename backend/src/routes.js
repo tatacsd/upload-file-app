7b67bb5b844cdc69f6dc1e3ca5fd560191ab683f
thays <thayscasado@hotmail.com>
2021-09-16 20:35:07 -0700

    upload the image on the local folder

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,13 @@
 // create the routes
 const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
 // define the routes
-routes.get("/", (req, res) => {
+// add multer as middleware - uploads files
+routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
+  // where multer adds the file to req.file
+  console.log(req.file);
   return res.json({
     message: "Welcome to the backend API!",
   });
