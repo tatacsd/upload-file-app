fe04e7b25fbe7cd5bbd43358b01669bfe2bc18da
thays <thayscasado@hotmail.com>
2021-09-16 21:22:25 -0700

    added post route to create a post

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,14 +2,20 @@
 const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
+const Post = require("./models/Post");
 
 // define the routes
 // add multer as middleware - uploads files
-routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
-  // where multer adds the file to req.file
-  console.log(req.file);
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file;
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url: "",
+  });
   return res.json({
-    message: "Welcome to the backend API!",
+    post,
   });
 });
 
